417d52e39d48e3707d99343aef16b89df1f42b2c
Donald Kibet <chelashawdonald@yahoo.com>
2021-02-11 11:23:50 +0300

    integrating openmrs features

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,54 +1,53 @@
-const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack').default
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
+const singleSpaAngularWebpack = require("single-spa-angular/lib/webpack")
+  .default;
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+const ReplaceInFileWebpackPlugin = require("replace-in-file-webpack-plugin");
+const packageJson = require("./package.json");
 
 module.exports = (angularWebpackConfig, options) => {
-  const singleSpaWebpackConfig = singleSpaAngularWebpack(angularWebpackConfig, options)
+  const singleSpaWebpackConfig = singleSpaAngularWebpack(
+    angularWebpackConfig,
+    options
+  );
 
-  singleSpaWebpackConfig.externals['@openmrs/esm-api'] = '@openmrs/esm-api';
-  singleSpaWebpackConfig.plugins.push(new BundleAnalyzerPlugin());
-  // console.log('webpack', singleSpaWebpackConfig);
-
-  if (singleSpaWebpackConfig.mode === 'production') {
-    const path = singleSpaWebpackConfig.output.path;
-    const publicPath = singleSpaWebpackConfig.output.publicPath;
-    console.log('path', path);
-    console.log('publicPath', publicPath);
-    // HACK to fix the deployment of fonts for bootstrap
-    // NOTE: this does not work with ng server --prod true, because the file does not exist
-    singleSpaWebpackConfig.plugins.push(
-      new ReplaceInFileWebpackPlugin([{
-        dir: path,
-        files: ['main-es2015.js'],
-        rules: [
-          {
-            search: 'glyphicons-halflings-regular.eot',
-            replace: publicPath + 'glyphicons-halflings-regular.eot'
-          },
-          {
-            search: 'glyphicons-halflings-regular.woff',
-            replace: publicPath + 'glyphicons-halflings-regular.woff'
-          },
-          // {
-          //   search: 'glyphicons-halflings-regular.woff2',
-          //   replace: publicPath + 'glyphicons-halflings-regular.woff2'
-          // },
-          {
-            search: 'glyphicons-halflings-regular.ttf',
-            replace: publicPath + 'glyphicons-halflings-regular.ttf'
-          },
-          {
-            search: 'glyphicons-halflings-regular.svg',
-            replace: publicPath + 'glyphicons-halflings-regular.svg'
-          }
-        ]
-      }]
-      ));
-
+  for (const dependency of Object.keys(packageJson.peerDependencies)) {
+    singleSpaWebpackConfig.externals[dependency] = dependency;
   }
 
-  // singleSpaWebpackConfig.module.rules.push({parser: {system: false}})
+  singleSpaWebpackConfig.plugins.push(new BundleAnalyzerPlugin());
 
+  if (singleSpaWebpackConfig.mode === "production") {
+    const path = singleSpaWebpackConfig.output.path;
+    const publicPath = singleSpaWebpackConfig.output.publicPath;
+    // NOTE: this does not work with ng server --prod true, because the file does not exist
+    singleSpaWebpackConfig.plugins.push(
+      new ReplaceInFileWebpackPlugin([
+        {
+          dir: path,
+          files: ["main-es2015.js"],
+          rules: [
+            {
+              search: "glyphicons-halflings-regular.eot",
+              replace: publicPath + "glyphicons-halflings-regular.eot",
+            },
+            {
+              search: "glyphicons-halflings-regular.woff",
+              replace: publicPath + "glyphicons-halflings-regular.woff",
+            },
+            {
+              search: "glyphicons-halflings-regular.ttf",
+              replace: publicPath + "glyphicons-halflings-regular.ttf",
+            },
+            {
+              search: "glyphicons-halflings-regular.svg",
+              replace: publicPath + "glyphicons-halflings-regular.svg",
+            },
+          ],
+        },
+      ])
+    );
+  }
   // Feel free to modify this webpack config however you'd like to
-  return singleSpaWebpackConfig
-}
+  return singleSpaWebpackConfig;
+};
---
+++ src/openmrs.ts
@@ -0,0 +1,24 @@
+import { backendDependencies } from "./openmrs-backend-dependencies";
+
+const importTranslation = require.context(
+  "../translations",
+  false,
+  /.json$/,
+  "lazy"
+);
+
+function setupOpenMRS() {
+  const moduleName = "@openmrs/esm-form-entry";
+
+  return {
+    extensions: [
+      {
+        id: "form-widget",
+        slot: "form-widget",
+        load: () => import("./main.single-spa"),
+      },
+    ],
+  };
+}
+
+export { backendDependencies, importTranslation, setupOpenMRS };
