c58ee2f972d9c79713c426054153f13ee8c90169
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2021-05-26 15:42:26 +0200

    Formatted file.

--- src/app/openmrs-api/openmrs-esm-api.service.ts
+++ src/app/openmrs-api/openmrs-esm-api.service.ts
@@ -1,21 +1,22 @@
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
-import {getCurrentUser, openmrsObservableFetch} from '@openmrs/esm-framework';
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { map } from "rxjs/operators";
+import { getCurrentUser, openmrsObservableFetch } from "@openmrs/esm-framework";
 
 @Injectable()
 export class OpenmrsEsmApiService {
-
-  constructor() { }
+  constructor() {}
 
   public getCurrentUser(): Observable<any> {
     return getCurrentUser();
   }
-  public  openmrsFetch(url): Observable<any> {
+  public openmrsFetch(url): Observable<any> {
     return openmrsObservableFetch(url);
   }
 
   public getCurrentUserLocation(): Observable<any> {
-    return this.openmrsFetch('/ws/rest/v1/appui/session').pipe(map(resp => resp.data));
+    return this.openmrsFetch("/ws/rest/v1/appui/session").pipe(
+      map((resp) => resp.data)
+    );
   }
 }
