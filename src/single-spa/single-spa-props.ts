1801fb1a877f7235ae02d47f598e778c289f1d38
Emmanuel Nyachoke <achachiez@gmail.com>
2021-07-12 12:59:14 +0300

    Upgrade single-spa

--- src/single-spa/single-spa-props.ts
+++ src/single-spa/single-spa-props.ts
@@ -6,10 +6,11 @@ export const singleSpaPropsSubject = new ReplaySubject<SingleSpaProps>(1);
 // Add any custom single-spa props you have to this type def
 // https://single-spa.js.org/docs/building-applications.html#custom-props
 export type SingleSpaProps = AppProps & {
-  formUuid: string;
-  encounterUuid?: string;
-  visitUuid?: string;
-  view: string;
-  closeWorkspace: () => void;
-  patient: any;
-};
+    formUuid: string;
+    encounterUuid?: string;
+    visitUuid?: string;
+    view: string;
+    closeWorkspace: () => void;
+    patient: any;
+  };
+  
